47270b944c3df361c9fc2e03e29e81088856a1aa
ManitejChowdary1 <2300031033@kluniversity.in>
2025-11-17 14:59:15 +0530

    in

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/END-SEM-/', // 👈 your repo name
+  base: '/END-SEM/', // 👈 your repo name
 })
